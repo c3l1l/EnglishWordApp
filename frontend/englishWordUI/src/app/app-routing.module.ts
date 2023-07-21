909b3334f2ef3f1340b89b671669871b2700f075
Celil Ozturk <celil.ozturk@windowslive.com>
2023-07-21 12:58:14 +0200

    bootstrap

--- frontend/englishWordUI/src/app/app-routing.module.ts
+++ frontend/englishWordUI/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './layouts/layout.component';
+import { HomeComponent } from './home/home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path:'',redirectTo:'home',pathMatch:'full'
+},
+  {
+   path:'home',component:LayoutComponent,
+   children:[
+   {path:'',component:HomeComponent}
+   ]
+  },
+  {
+    path:'**',component:NotFoundComponent
+  }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/englishWordUI/src/app/app.module.ts
+++ frontend/englishWordUI/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { SidebarComponent } from './layouts/sidebar/sidebar.component';
+import { HomeComponent } from './home/home/home.component';
+import { NavbarComponent } from './layouts/navbar/navbar.component';
+import { LayoutComponent } from './layouts/layout.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    SidebarComponent,
+    HomeComponent,
+    NavbarComponent,
+    LayoutComponent,
+    NotFoundComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
